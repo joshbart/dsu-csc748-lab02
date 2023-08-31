9a2e027b40f75be94611501677bc06de07fa21da
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-30 21:46:21 -0500

    Added more common structure.

--- lab2-1.py
+++ lab2-1.py
@@ -2,4 +2,10 @@ import pwn
 
 if __name__ == "__main__":
     
-    process_to_exploit = pwn.remote("csc748.hostbin.org", 7021)
+    # I run the process.
+    # process_to_exploit = pwn.remote("csc748.hostbin.org", 7021)
+    # This next line can replace the one above for testing against a local binary.
+    process_to_exploit = pwn.process("./lab2-1.bin")
+
+    # Now I can drop into a shell.
+    process_to_exploit.interactive()
